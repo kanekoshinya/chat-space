3b99f338f7fe9a52fea7ccec69a7098840544cc9
kanekoshinya <knksny777@icloud.com>
2020-02-14 16:11:35 +0900

    コードの修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,46 +2,43 @@ $(function(){
   function buildHTML(message){
     if (message.image){
       var html =
-      `<div class="messages">
-      <div class="message">
-      <div class="message__name">
-      <div class="message__name--name">
-      ${message.user_name}
-      </div>
-      <div class="message__name--day">
-      ${message.created_at}
-      </div>
-      </div>
-      <div class="message__coments">
-      <p class="lower-message__content">
-      ${message.content}
-      </p>
-      
-      </div>
-      </div>
-      <img src=${message.image} >
+     `<div class="messages">
+        <div class="message">
+          <div class="message__name">
+            <div class="message__name--name">
+              ${message.user_name}
+            </div>
+            <div class="message__name--day">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__coments">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>
+        <img src=${message.image} >
       </div>`
       return html;
     }else{
       var html =
-      `<div class="messages">
-      <div class="message">
-      <div class="message__name">
-      <div class="message__name--name">
-      ${message.user_name}
-      </div>
-      <div class="message__name--day">
-      ${message.created_at}
-      </div>
-      </div>
-      <div class="message__coments">
-      <p class="lower-message__content">
-      ${message.content}
-      </p>
-      
-      </div>
-      </div>
-      
+     `<div class="messages">
+        <div class="message">
+          <div class="message__name">
+            <div class="message__name--name">
+              ${message.user_name}
+            </div>
+            <div class="message__name--day">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message__coments">
+            <p class="lower-message__content">
+              ${message.content}
+            </p> 
+          </div>
+        </div> 
       </div>`
       return html;
     };
@@ -63,10 +60,11 @@ $(function(){
        $('.messages').append(html);
        $('.chat-main__message-list').animate({ scrollTop: $('.messages')[0].scrollHeight});      
        $('form')[0].reset();
+       $('.submit-btn').prop('disabled', false);
      })
      .fail(function() {
       alert("メッセージ送信に失敗しました");
+      $('.submit-btn').prop('disabled', false);
      })
-     return false;
   });
 });
